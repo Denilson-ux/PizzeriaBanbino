567eb9fd9139cff011b7d32046ee14b3f3f57fc9
Denilson-ux <denilsonquichu@gmail.com>
2025-11-03 10:07:24 -0400

    feat(recetas): add admin routes to manage recipe per ItemMenu directly on main

---
+++ routes/recetas.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RecetaController;
+
+// Rutas para gestionar receta por item
+Route::middleware(['auth'])->prefix('admin')->group(function () {
+    Route::get('item-menu/{id}/receta', [RecetaController::class, 'show'])->name('receta.show');
+    Route::post('item-menu/{id}/receta', [RecetaController::class, 'agregarIngrediente'])->name('receta.agregar');
+    Route::put('item-menu/{id}/receta/{idIngrediente}', [RecetaController::class, 'actualizarIngrediente'])->name('receta.actualizar');
+    Route::delete('item-menu/{id}/receta/{idIngrediente}', [RecetaController::class, 'eliminarIngrediente'])->name('receta.eliminar');
+});
