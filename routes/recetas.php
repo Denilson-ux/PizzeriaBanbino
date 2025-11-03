c3c4084783503bba26c91038e3cf9f794a891d1c
Denilson-ux <denilsonquichu@gmail.com>
2025-11-03 10:47:34 -0400

    fix(routes): remove duplicated admin prefix from recetas.php so routes resolve as /admin/item-menu/{id}/receta

--- routes/recetas.php
+++ routes/recetas.php
@@ -3,9 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RecetaController;
 
-Route::middleware(['web','auth'])->prefix('admin')->group(function () {
-    Route::get('item-menu/{id}/receta', [RecetaController::class, 'show'])->name('receta.show');
-    Route::post('item-menu/{id}/receta', [RecetaController::class, 'agregarIngrediente'])->name('receta.agregar');
-    Route::put('item-menu/{id}/receta/{idIngrediente}', [RecetaController::class, 'actualizarIngrediente'])->name('receta.actualizar');
-    Route::delete('item-menu/{id}/receta/{idIngrediente}', [RecetaController::class, 'eliminarIngrediente'])->name('receta.eliminar');
-});
+// SIN prefix/middleware: se heredan del grupo /admin de routes/web.php
+Route::get('item-menu/{id}/receta', [RecetaController::class, 'show'])->name('receta.show');
+Route::post('item-menu/{id}/receta', [RecetaController::class, 'agregarIngrediente'])->name('receta.agregar');
+Route::put('item-menu/{id}/receta/{idIngrediente}', [RecetaController::class, 'actualizarIngrediente'])->name('receta.actualizar');
+Route::delete('item-menu/{id}/receta/{idIngrediente}', [RecetaController::class, 'eliminarIngrediente'])->name('receta.eliminar');
